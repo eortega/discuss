fb1f70d2da2f4202422077da5a9ae1c8073fec7f
Edgar Ortega <eortegamartinez@gmail.com>
2018-03-14 11:15:48 -0600

    both views and route  to create a topic

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,7 +1,14 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def new(conn, _params) do
-    render conn, "index.html"
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
   end
 end
